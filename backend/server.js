006222a2169906fcdc7c0131b4b17e3422f03d54
C0rtaz <120026948+C0rtaz@users.noreply.github.com>
2024-04-30 17:52:28 +0700

    serv

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,6 @@
 const express = require('express')
 const mysql = require('mysql2');
 const app = express()
-// const cors = require('cors');
-// app.use(cors());
-
-
-
-//app.use(express.static("public"))
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-  
-//     if (req.method === 'OPTIONS') {
-//       res.sendStatus(204);
-//     } else {
-//       next();
-//     }
-//   });
 app.use(express.json());
 const port = 5000;
 app.listen(port, () => {
@@ -31,27 +14,12 @@ const connection = mysql.createConnection({
     password: 'root'
   });
 
-  async function runQuery(query, params) {
-    try {
-      // Подготавливаем запрос
-      const preparedStatement = connection.prepare(query);
-      // Выполняем подготовленный запрос с параметрами
-      const [results] = await preparedStatement.execute(params);
-      // Закрываем подготовленный запрос
-      preparedStatement.finalize();
-      return results;
-    } catch (error) {
-      // Обработка ошибок
-      console.error('Ошибка при выполнении запроса:', error);
-      throw error;
-    }
-  }
 app.post('/auth', (req, res) => { 
     const login =  req.body.log;
     const password =  req.body.pass;
     const user = [login, password];
     //res.status(200).json({ message: user });
-    connection.query('SELECT * FROM web.users', function (error, results, fields) {
+    connection.query('SELECT * FROM web.users where login = ? and password = ?', user, function (error, results, fields) {
 
             if (error) {
                 res.send({ express: error }); 
@@ -64,41 +32,66 @@ app.post('/auth', (req, res) => {
     
   }); 
 
-
-
-
-//   app.get('/auth', (req, res) => {
-//     res.send("ok");
-//     res.json('message: kill me');
-//     const { login, password } = req.body;
-    //res.status(200).send({ message: 'User authenticated.', body: req.body });
+  app.post('/addUser',(req, res) => { 
+    const user = [
+      req.body.name,
+      req.body.surname, 
+      req.body.secsurname,
+      req.body.phone,
+      req.body.login, 
+      req.body.password,
+      req.body.roleID
+    ]
+    const query = 'INSERT INTO web.users (name, surname, secsurname, phone, login, password, roleID) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    connection.query(query, user, function (error, results, fields) {
+            if (error) {
+                res.send({ express: error }); 
+                return;
+            }
+            res.status(200).json({ message: "success" });
+            
+        });
     
-    // const { login, password } = req.body;
-    // if (login === 'admin' && password === 'secret') {
-    //   // Авторизуйте пользователя
-    //   // ...
-    //   res.status(200).send({ message: 'User authenticated.' });
-    // } else {
-    //   // Неверные учетные данные
-    //   res.status(401).send({ message: 'Invalid credentials.' });
-    // }
- // });
-  
-
-// app.get('/auth', (req, res) => { 
-
-//     console.log(req.body);
-//     // const { logInput, passInput } = req.body;
-//     // connection.query('SELECT * FROM web.users WHERE login = ? and password = ?', [logInput, passInput], function (error, results, fields) {
-//     // if (error) {
-//     //     res.send({ express: error }); 
-//     //     return;
-//     // }
-//     // res.send({ results });
-//     // console.log(results);
-//   //});
     
-//  });
-
+  });
   
+  app.post('/addSchedule',(req, res) => { 
+    const schedule = [
+      req.body.serviceID,
+      req.body.timeStart, 
+      req.body.timeEnd,
+      req.body.teacherName,
+    ]
+    const query = 'INSERT INTO web.schedule (serviceID, timeStart, timeEnd, teacherName) VALUES (?, ?, ?, ?)';
+    connection.query(query, schedule, function (error, results, fields) {
+            if (error) {
+                res.send({ express: error }); 
+                return;
+            }
+            res.status(200).json({ message: "success" });
+            
+        });
+    
+    
+  }); 
+  app.post('/addStudent',(req, res) => { 
+    const student = [
+      req.body.userID,
+      req.body.name, 
+      req.body.surname,
+      req.body.secsurname,
+    ]
+
+    const query = 'INSERT INTO web.student (userID, name, surname, secsurname) VALUES (?, ?, ?, ?)';
+    connection.query(query, student, function (error, results, fields) {
+            if (error) {
+                res.send({ express: error }); 
+                return;
+            }
+            res.status(200).json({ message: "success" });
+            
+        });
+    
+    
+  }); 
   
